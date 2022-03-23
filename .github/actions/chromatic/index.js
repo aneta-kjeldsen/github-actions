58a0d058edc77ff94a1f5853e838ba31cea25d15
Aneta Kjeldsen <aneta.kjeldsen@maersk.com>
2022-03-23 20:05:12 +0100

    test annotations

--- .github/actions/chromatic/index.js
+++ .github/actions/chromatic/index.js
@@ -4,14 +4,17 @@ const execSync = require("child_process").execSync;
 import createCheck from "create-check";
 
 try {
-  const jobOutput = execSync(`npm -v`).toString().trim();
+  const jobOutput = execSync(`npm -v
+  node -v`)
+    .toString()
+    .trim();
   core.setOutput("job_output", jobOutput);
 
   const main = async () => {
     await createCheck({
       tool: "stylelint",
       name: "Check Styles for Errors",
-      annotations: createAnnotations(results),
+      annotations: "test",
       errorCount,
       warningCount,
       appId: APP_ID,
