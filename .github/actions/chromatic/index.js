e4135fdf5ccb6b6f3d52c4500948d71075970932
Aneta Kjeldsen <aneta.kjeldsen@maersk.com>
2022-03-23 21:05:21 +0100

    try to use octokit

--- .github/actions/chromatic/index.js
+++ .github/actions/chromatic/index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const execSync = require("child_process").execSync;
-import { Octokit, App } from "octokit";
+import { Octokit } from "octokit/rest";
 
 try {
   const jobOutput = execSync(`npm -v
@@ -14,6 +14,24 @@ try {
     data: { login },
   } = await octokit.rest.users.getAuthenticated();
   console.log("Hello, %s", login);
+  errorCount = 0;
+  const check = {
+    owner,
+    repo,
+    completed_at: new Date().toISOString(),
+    head_sha: HEAD,
+    conclusion: (errorCount > 0 && "failure") || "success",
+    output: {
+      title: `${tool} Results`,
+      summary,
+      annotations: first,
+    },
+  };
+  await octokit.checks.create(check).catch(async (error) => {
+    console.log("Failed to create check with error:", error);
+    const PRE_HEAD = (await execa("git", ["rev-parse", "HEAD^1"])).stdout;
+    return octokit.checks.create({ ...check, head_sha: PRE_HEAD });
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
