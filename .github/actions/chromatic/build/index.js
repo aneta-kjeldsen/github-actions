ef7d277bc06bf75507f3ef234006252fff29d7ae
Aneta Kjeldsen <aneta.kjeldsen@maersk.com>
2022-03-24 09:54:40 +0100

    update comment

--- .github/actions/chromatic/build/index.js
+++ .github/actions/chromatic/build/index.js
@@ -27,18 +27,22 @@ try {
   // } = await octokit.rest.users.getAuthenticated();
   // console.log("Hello, %s", login);
 
-  const { data: pullRequest } = await octokit.rest.pulls.get({
-    owner: "aneta-kjeldsen",
-    repo: "github-actions",
-    pull_number: 1,
-  });
-  console.log("pullRequest", pullRequest);
-  const { data: comment } = octokit.rest.repos.updateCommitComment({
-    owner: "aneta-kjeldsen",
-    repo: "github-actions",
-    comment_id: "1076761106",
-    body: "new body",
-  });
+  // const { data: pullRequest } = await octokit.rest.pulls.get({
+  //   owner: "aneta-kjeldsen",
+  //   repo: "github-actions",
+  //   pull_number: 1,
+  // });
+  // console.log("pullRequest", pullRequest);
+
+  const { data: comment } = await octokit.request(
+    "PATCH /repos/{owner}/{repo}/comments/{comment_id}",
+    {
+      owner: "aneta-kjeldsen",
+      repo: "github-actions",
+      comment_id: 1076761106,
+      body: "new body",
+    }
+  );
   console.log("comment", comment);
 } catch (error) {
   core.setFailed(error.message);
