074bbd624c2bbe9e7005a756dc9432a56ff276e7
Aneta Kjeldsen <aneta.kjeldsen@maersk.com>
2022-03-24 09:49:23 +0100

    use diferent token

--- index.js
+++ index.js
@@ -24,17 +24,17 @@ var token = jwt.sign(
 // );
 
 // YOUR_INSTALLATION_ACCESS_TOKEN,ID: 24376381
-const getAccessToken = async () => {
-  const octokit = new Octokit({ auth: token });
-  return await octokit.request(
-    "POST /app/installations/{installation_id}/access_tokens",
-    {
-      installation_id: 24376381,
-      repositories: ["repositories"],
-    }
-  );
-};
-console.log(getAccessToken());
+// const getAccessToken = async () => {
+//   const octokit = new Octokit({ auth: token });
+//   return await octokit.request(
+//     "POST /app/installations/{installation_id}/access_tokens",
+//     {
+//       installation_id: 24376381,
+//       repositories: ["repositories"],
+//     }
+//   );
+// };
+// console.log(getAccessToken());
 // console.log(
 //   `curl -i -X POST -H "Authorization: Bearer ${token}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/aneta-jeldsen/github-actions/check-runs`
 // );
