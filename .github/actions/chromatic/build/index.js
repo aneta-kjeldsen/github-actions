e46283f3a03f4b1cf5c7600f6cbbfea1e3072249
Aneta Kjeldsen <aneta.kjeldsen@maersk.com>
2022-03-24 10:23:58 +0100

    testing pr body

--- .github/actions/chromatic/build/index.js
+++ .github/actions/chromatic/build/index.js
@@ -34,23 +34,33 @@ try {
   // });
   // console.log("pullRequest", pullRequest);
 
-  await octokit.request(
-    "PATCH /repos/{owner}/{repo}/pulls/comments/{comment_id}",
+  // await octokit.request(
+  //   "PATCH /repos/{owner}/{repo}/pulls/comments/{comment_id}",
+  //   {
+  //     owner: "aneta-kjeldsen",
+  //     repo: "github-actions",
+  //     comment_id: 1077397270,
+  //     body: "new body",
+  //   }
+  // );
+
+  const { data: pullRequest } = await octokit.request(
+    "GET /repos/{owner}/{repo}/pulls/{pull_number}",
     {
       owner: "aneta-kjeldsen",
       repo: "github-actions",
-      comment_id: 1077397270,
-      body: "new body",
+      pull_number: 1,
     }
   );
+  const pullRequestBody = pullRequest.body;
+  console.log(pullRequestBody);
 
-  // await octokit.request("PATCH /repos/{owner}/{repo}/pulls/{pull_number}", {
-  //   owner: "aneta-kjeldsen",
-  //   repo: "github-actions",
-  //   pull_number: 1,
-  //   title: "title",
-  //   body: "my new body",
-  // });
+  await octokit.request("PATCH /repos/{owner}/{repo}/pulls/{pull_number}", {
+    owner: "aneta-kjeldsen",
+    repo: "github-actions",
+    pull_number: 1,
+    body: pullRequestBody,
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
