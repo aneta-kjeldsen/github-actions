07b11033690cd0a352071a4077d71651a3c25df1
Aneta Kjeldsen <aneta.kjeldsen@maersk.com>
2022-03-24 10:00:59 +0100

    change pr body text

--- .github/actions/chromatic/index.js
+++ .github/actions/chromatic/index.js
@@ -17,18 +17,18 @@ try {
   // } = await octokit.rest.users.getAuthenticated();
   // console.log("Hello, %s", login);
 
-  const { data: pullRequest } = await octokit.rest.pulls.get({
-    owner: "aneta-kjeldsen",
-    repo: "github-actions",
-    pull_number: 1,
-  });
-  console.log("pullRequest", pullRequest);
-  const { data: comment } = await octokit.rest.repos.updateCommitComment({
-    owner: "aneta-kjeldsen",
-    repo: "github-actions",
-    comment_id: 1076761106,
-    body: "new body",
-  });
+  // const { data: pullRequest } = await octokit.rest.pulls.get({
+  //   owner: "aneta-kjeldsen",
+  //   repo: "github-actions",
+  //   pull_number: 1,
+  // });
+  // console.log("pullRequest", pullRequest);
+  // const { data: comment } = await octokit.rest.repos.updateCommitComment({
+  //   owner: "aneta-kjeldsen",
+  //   repo: "github-actions",
+  //   comment_id: 1076761106,
+  //   body: "new body",
+  // });
 
   // await octokit.request('PATCH /repos/{owner}/{repo}/comments/{comment_id}', {
   //   owner: "aneta-kjeldsen",
@@ -36,7 +36,7 @@ try {
   //   comment_id: 1076761106,
   //   body: "new body"
   // })
-  console.log("comment", comment);
+  // console.log("comment", comment);
 } catch (error) {
   core.setFailed(error.message);
 }
