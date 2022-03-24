18d05a64f5125997572289f0349ab52103f49304
marcin-sieradzki-maersk <marcin.sieradzki@maersk.com>
2022-03-24 14:59:49 +0100

    rebuild chromatic action

--- .github/actions/chromatic/build/index.js
+++ .github/actions/chromatic/build/index.js
@@ -1758,7 +1758,7 @@ try {
   const output = `[View Storybook](${storybookDetails}) | [Visual regression build results](https://www.chromatic.com/build?${buildDetails})`;
   core.setOutput("job_output", output);
   const diff = execSync("git diff --name-only HEAD^ HEAD").toString();
-  console.log(diff);
+  console.log("diff", diff);
 } catch (error) {
   core.setFailed(error.message);
 }
